d770e92494d064ba177f512ddb8411d90e837fc7
mudler <mudler@mocaccino.org>
2023-06-07 00:53:34 +0200

    Add mmap, mlock, tensorsplit and maingpu options

--- binding.h
+++ binding.h
@@ -14,7 +14,7 @@ int eval(void* params_ptr, void *ctx, char*text);
 
 void save_state(void *ctx, char *dst, char*modes);
 
-void* load_model(const char *fname, int n_ctx, int n_seed, bool memory_f16, bool mlock, bool embeddings, int n_gpu);
+void* load_model(const char *fname, int n_ctx, int n_seed, bool memory_f16, bool mlock, bool embeddings, bool mmap, int n_gpu, int n_batch, const char *maingpu, const char *tensorsplit);
 
 int get_embeddings(void* params_ptr, void* state_pr, float * res_embeddings);
 
@@ -24,7 +24,7 @@ void* llama_allocate_params(const char *prompt, int seed, int threads, int token
                             int top_k, float top_p, float temp, float repeat_penalty, 
                             int repeat_last_n, bool ignore_eos, bool memory_f16, 
                             int n_batch, int n_keep, const char** antiprompt, int antiprompt_count,
-                            float tfs_z, float typical_p, float frequency_penalty, float presence_penalty, int mirostat, float mirostat_eta, float mirostat_tau, bool penalize_nl, const char *logit_bias, const char *session_file, bool prompt_cache_all);
+                            float tfs_z, float typical_p, float frequency_penalty, float presence_penalty, int mirostat, float mirostat_eta, float mirostat_tau, bool penalize_nl, const char *logit_bias, const char *session_file, bool prompt_cache_all, bool mlock, bool mmap, const char *maingpu, const char *tensorsplit );
 
 void llama_free_params(void* params_ptr);
 
