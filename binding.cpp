c55308a1b5aa7c3dfa353a431889281b6dcd994e
mudler <mudler@mocaccino.org>
2023-06-17 00:01:20 +0200

    Workaround llama_init_from_file parameter copy
    This is needed until https://github.com/ggerganov/llama.cpp/pull/1902 is
    addressed/merged.
    
    Signed-off-by: mudler <mudler@mocaccino.org>

--- binding.cpp
+++ binding.cpp
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "llama.h"
+
 #include "binding.h"
 
 #include <cassert>
@@ -630,7 +631,7 @@ void* load_model(const char *fname, int n_ctx, int n_seed, bool memory_f16, bool
     llama_init_backend();
     void* res = nullptr;
     try {
-        res = llama_init_from_file(fname, lparams);
+        res = llama_init_from_file(fname, &lparams);
     } catch(std::runtime_error& e) {   
         fprintf(stderr, "failed %s",e.what());
         return res;
