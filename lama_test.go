0274e0f17318adf53f12e5f6a55ff5522c45feac
mudler <mudler@localai.io>
2023-07-27 18:34:28 +0200

    Add a small test with openLLaMa
    Signed-off-by: mudler <mudler@localai.io>

--- lama_test.go
+++ lama_test.go
@@ -1,6 +1,8 @@
 package llama_test
 
 import (
+	"os"
+
 	. "github.com/go-skynet/go-llama.cpp"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -12,6 +14,20 @@ var _ = Describe("LLama binding", func() {
 			model, err := New("not-existing")
 			Expect(err).To(HaveOccurred())
 			Expect(model).To(BeNil())
+		})
+	})
+	Context("Inferencing", func() {
+		It("Works with "+os.Getenv("TEST_MODEL"), func() {
+			model, err := New(os.Getenv("TEST_MODEL"), EnableF16Memory, SetContext(128), SetMMap(true))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(model).ToNot(BeNil())
+			text, err := model.Predict(`Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction: How much is 2+2?
+
+### Response: `)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(text).To(ContainSubstring("4"))
 		})
 	})
 })
