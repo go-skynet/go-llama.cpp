9c3ac7633f3583435a53867823cabc480564523f
scott4290 <scott3091@gmail.com>
2023-08-07 18:41:14 -0400

    go-llama.cpp Update llama.go - Fix memory leaks
    Changed C.free to defer C.free.

--- llama.go
+++ llama.go
@@ -24,7 +24,7 @@ type LLama struct {
 func New(model string, opts ...ModelOption) (*LLama, error) {
 	mo := NewModelOptions(opts...)
 	modelPath := C.CString(model)
-	defer C.free(unsafe.Pointer(modelPath))
+	defer C.free(unsafe.Pointer(defer modelPath))
 	result := C.load_model(modelPath,
 		C.int(mo.ContextSize), C.int(mo.Seed),
 		C.bool(mo.F16Memory), C.bool(mo.MLock), C.bool(mo.Embeddings), C.bool(mo.MMap), C.bool(mo.LowVRAM),
@@ -32,7 +32,6 @@ func New(model string, opts ...ModelOption) (*LLama, error) {
 		C.float(mo.FreqRopeBase), C.float(mo.FreqRopeScale),
 		C.float(mo.RMSNormEPS), C.int(mo.GQA),
 	)
-	C.free(unsafe.Pointer(modelPath))  // free allocated C string
 	
 	if result == nil {
 		return nil, fmt.Errorf("failed loading model")
@@ -51,8 +50,8 @@ func (l *LLama) LoadState(state string) error {
 	w := C.CString("rb")
 	result := C.load_state(l.state, d, w)
 
-	C.free(unsafe.Pointer(d))  // free allocated C string
-	C.free(unsafe.Pointer(w))  // free allocated C string
+	defer C.free(unsafe.Pointer(d))  // free allocated C string
+	defer C.free(unsafe.Pointer(w))  // free allocated C string
 
 	if result != 0 {
 		return fmt.Errorf("error while loading state")
@@ -67,8 +66,8 @@ func (l *LLama) SaveState(dst string) error {
 
 	C.save_state(l.state, d, w)
 
-	C.free(unsafe.Pointer(d))  // free allocated C string
-	C.free(unsafe.Pointer(w))  // free allocated C string
+	defer C.free(unsafe.Pointer(d))  // free allocated C string
+	defer C.free(unsafe.Pointer(w))  // free allocated C string
 	
 	_, err := os.Stat(dst)
 	return err
