7b5826a6dd23f192754252e3c2e67ff7502afcdd
Dave Lee <dave@gray101.com>
2023-08-14 17:26:04 -0400

    Skip() instead of if block for readability

--- llama_test.go
+++ llama_test.go
@@ -1,7 +1,6 @@
 package llama_test
 
 import (
-	"fmt"
 	"os"
 
 	. "github.com/go-skynet/go-llama.cpp"
@@ -19,41 +18,47 @@ var _ = Describe("LLama binding", func() {
 			Expect(model).To(BeNil())
 		})
 	})
-	if testModelPath != "" {
-		Context("Inferencing tests (using "+testModelPath+") ", func() {
+	Context("Inferencing tests (using "+testModelPath+") ", func() {
+		getModel := func() (*LLama, error) {
+			model, err := New(
+				testModelPath,
+				EnableF16Memory,
+				SetContext(128),
+				SetMMap(true),
+				SetNBatch(512),
+			)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(model).ToNot(BeNil())
+			return model, err
+		}
 
-			getModel := func() (*LLama, error) {
-				model, err := New(
-					testModelPath,
-					EnableF16Memory,
-					SetContext(128),
-					SetMMap(true),
-					SetNBatch(512),
-				)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(model).ToNot(BeNil())
-				return model, err
+		It("predicts successfully", func() {
+			if testModelPath == "" {
+				Skip("test skipped - only makes sense if testModelPath is set.")
 			}
 
-			It("predicts successfully", func() {
-				model, err := getModel()
-				text, err := model.Predict(`Below is an instruction that describes a task. Write a response that appropriately completes the request.
+			model, err := getModel()
+			text, err := model.Predict(`Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
-	### Instruction: How much is 2+2?
+### Instruction: How much is 2+2?
 
-	### Response: `, SetRopeFreqBase(10000.0), SetRopeFreqScale(1))
-				Expect(err).ToNot(HaveOccurred())
-				Expect(text).To(ContainSubstring("4"))
-			})
-			It("tokenizes strings successfully", func() {
-				model, err := getModel()
-				l, tokens, err := model.TokenizeString("A STRANGE GAME.\nTHE ONLY WINNING MOVE IS NOT TO PLAY.\n\nHOW ABOUT A NICE GAME OF CHESS?",
-					SetRopeFreqBase(10000.0), SetRopeFreqScale(1))
-
-				Expect(err).ToNot(HaveOccurred())
-				Expect(l).To(BeNumerically(">", 0))
-				Expect(int(l)).To(Equal(len(tokens)))
-			})
+### Response: `, SetRopeFreqBase(10000.0), SetRopeFreqScale(1))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(text).To(ContainSubstring("4"))
 		})
-	}
+
+		It("tokenizes strings successfully", func() {
+			if testModelPath == "" {
+				Skip("test skipped - only makes sense if testModelPath is set.")
+			}
+
+			model, err := getModel()
+			l, tokens, err := model.TokenizeString("A STRANGE GAME.\nTHE ONLY WINNING MOVE IS NOT TO PLAY.\n\nHOW ABOUT A NICE GAME OF CHESS?",
+				SetRopeFreqBase(10000.0), SetRopeFreqScale(1))
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(l).To(BeNumerically(">", 0))
+			Expect(int(l)).To(Equal(len(tokens)))
+		})
+	})
 })
