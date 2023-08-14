5175137be37f51da50c038209f234c1918e78877
Dave <dave@gray101.com>
2023-08-14 17:08:00 -0400

    Update llama_test.go
    remove temporary text token dump now that things work.
    
    Co-authored-by: Ettore Di Giacinto <mudler@users.noreply.github.com>

--- llama_test.go
+++ llama_test.go
@@ -52,7 +52,6 @@ var _ = Describe("LLama binding", func() {
 
 				Expect(err).ToNot(HaveOccurred())
 				Expect(l).To(BeNumerically(">", 0))
-				fmt.Printf("Temp Token Dump: %+v", tokens)
 				Expect(int(l)).To(Equal(len(tokens)))
 			})
 		})
